ab9f30f53859dbeb48befcf1f77b7a59ce55ead8
ryakosh <ryakosh@gmail.com>
2018-04-01 04:21:31 +0000

    Implement AppBluryOverlay component and fix some page sizing issues

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
+    <app-blury-overlay v-model="overlay">
+      some content somecontent
+      some content somecontent
+      some content somecontent
+
+      <div slot="overlay">
+      some content somecontent
+      some content somecontent
+      </div>
+    </app-blury-overlay>
   </div>
 </template>
 
 <script>
+import AppBluryOverlay from "./components/AppBluryOverlay/AppBluryOverlay";
 export default {
   name: "app",
-  components: {}
+  components: {
+    AppBluryOverlay
+  },
+  data() {
+    return {
+      overlay: true
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 #app {
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/AppBluryOverlay/AppBluryOverlay.vue
@@ -0,0 +1,109 @@
+<!--
+  Props: {
+    show: "If true, overlay will appear, It's used with v-model" -> Boolean,
+  },
+  Events: {
+    on-close: "Emitted when overlay's close button is clicked,
+      It's used with v-model" -> (false: Boolean)
+  },
+  Slot: {
+    default: "Slot used for storing actual page content",
+    overlay: "Slot used for storing overlay's content"
+  },
+  v-model: "Control visibility of the overlay" -> Boolean
+-->
+
+<template>
+  <div :class="['abo-container', {show}]">
+    <div class="content">
+      <slot></slot>
+    </div>
+
+    <div class="overlay">
+      <div class="top clearfix">
+        <app-circulartton 
+        @click.native="$emit('on-close', false)" 
+        bg="#e53935">
+          <img src="./assets/close-overlay-white.svg" alt="Close button" />
+        </app-circulartton>
+      </div>
+
+      <div class="bottom">
+        <slot name="overlay"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppCirculartton from "../AppCirculartton/AppCirculartton";
+
+export default {
+  name: "AppBluryOverlay",
+  components: {
+    AppCirculartton
+  },
+  model: {
+    prop: "show",
+    event: "on-close"
+  },
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.abo-container {
+  width: 100%;
+  height: 100%;
+
+  .content {
+    width: inherit;
+    height: inherit;
+    filter: none;
+    opacity: none;
+    transition-property: filter, opacity;
+    transition-duration: .25s;
+  }
+
+  .overlay {
+    width: inherit;
+    height: inherit;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    display: none;
+  }
+
+    .top {
+      width: 100%;
+      padding: 5px;
+
+      button {
+        float: right;
+      }
+    }
+
+    .bottom {
+      width: 100%;
+      height: 100%;
+      padding: 5px;
+    }
+}
+
+.abo-container.show {
+  .content {
+    filter: blur(10px);
+    opacity: 0.4;
+  }
+
+  .overlay {
+    display: block;
+  }
+}
+</style>
