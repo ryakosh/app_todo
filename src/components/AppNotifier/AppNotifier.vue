70a047515fd71d38ae683e71ce5b1eacd23f3f7d
ryakosh <ryakosh@gmail.com>
2018-05-21 16:58:44 +0000

    Implement the AppNotifier component

---
+++ src/components/AppNotifier/AppNotifier.vue
@@ -0,0 +1,54 @@
+<!--
+  Props: {
+    show: "Used to control the visibility of the notifier" -> Boolean
+  },
+  Slots: {
+    default: "Used to place content in the notifier"
+  }
+-->
+
+<template>
+  <div :class="['an-container', {show}]">
+    <span><slot></slot></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNotifier",
+  props: {
+    show: {
+      required: true,
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$notifier-height: 20px;
+$notifier-padding: 20px;
+
+.an-container {
+  width: 100%;
+  height: $notifier-height;
+  padding: $notifier-padding;
+  background-color: #212121;
+  position: fixed;
+  bottom: -$notifier-height - $notifier-padding;
+  transition-property: bottom, box-shadow;
+  transition-duration: .25s;
+  color: white;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  box-shadow: none;
+
+  &.show {
+    bottom: 0;
+    box-shadow: 0 -1px 12px black;
+  }
+}
+</style>
